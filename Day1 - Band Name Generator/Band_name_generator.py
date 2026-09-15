e1cfe379f389fadff3a9137ef973ddbbb728524f
Sudipta Maiti <sudiptamaiti314@gmail.com>
2026-09-15 16:07:15 +0530

    Day 1 Complete

---
+++ Day1 - Band Name Generator/Band_name_generator.py
@@ -0,0 +1,4 @@
+print("WELCOME TO BAND NAME GENERATOR.")
+city=input("Which city did you grew up in ?\n")
+pet=input("What is the name of your pet ?\n")
+print("Your band name is " + city + " " + pet)
